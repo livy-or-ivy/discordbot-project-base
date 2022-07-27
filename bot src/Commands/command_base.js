531cf0ce171ad00c3c86d88b630695bbf1efe6c1
rosiee <97999463+rosiee-sz@users.noreply.github.com>
2022-07-27 20:33:05 -0300

    Update command_base.js

---
+++ bot src/Commands/command_base.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    //command code here
+}
+
+module.exports.config = {
+    name: "command name here",
+    aliases: ["command aliases here"]
+};
